c9800698edd68e5ae67cac7ade5a0823d075fc7a
sujoto <samuelschulmaester@gmail.com>
2024-01-21 02:45:16 -0300

    actualizando estilos

--- src/Components/Assets/all_product.js
+++ src/Components/Assets/all_product.js
@@ -2,23 +2,25 @@
 import driftnegra from './drift-negra.jpg';
 import driftfte from './drift-fte.jpg';
 import interstellar from './interstellar.jpg';
+import remeraLisa from './remera-lisa.jpg';
+import yokaiblanc from './yokai-remera-blanca.jpg';
 
 
 
 let all_product = [
     {
       id: 1,
-      name: 'Remera-drift-negra',
+      name: 'Remeradrift negra',
       description: 'Prenda de excelente confeccion',
-      price: 21.000,
+      price: 21.800,
       imageUrl:driftnegra,
 
     },
     {
       id: 2,
-      name: 'Producto 2',
+      name: 'Remera drift frente',
       description: 'Descripción del Producto 2',
-      price: 15.49,
+      price: 21.999,
       imageUrl: driftfte,
     },
 
@@ -28,6 +30,24 @@ let all_product = [
         description: 'Descripción del Producto 2',
         price: 15.49,
         imageUrl: interstellar,
+      },
+
+
+      {
+        id: 4,
+        name: 'Producto 2',
+        description: 'Descripción del Producto 2',
+        price: 15.49,
+        imageUrl: remeraLisa,
+      },
+
+
+      {
+        id: 5,
+        name: 'Producto 2',
+        description: 'Descripción del Producto 2',
+        price: 15.49,
+        imageUrl: yokaiblanc,
       },
     // Agrega más productos según sea necesario
   ];
